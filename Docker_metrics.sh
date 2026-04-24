eccd4148bb5e987e7ea39620356f118e4a477c98
shivam0915 <shivamshekhawat860@gmail.com>
2026-04-24 16:41:15 +0530

    Create Docker_metrics.sh

---
+++ Docker_metrics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONTAINER_IDS=$(docker ps -q)
+for id in $CONTAINER_IDS
+do
+  CPU=$(docker stats --no-stream --format "{{.CPUPerc}}" $id | tr -d '%')
+  MEM=$(docker stats --no-stream --format "{{.MemPerc}}" $id | tr -d '%')
+  if [ ! -z "$CPU" ]; then
+    # Changed dimension name to 'host' to match your alarm configuration
+    aws cloudwatch put-metric-data --namespace "Final-USE-CASE" --metric-name "DockerCPUUtilization" --value $CPU --dimensions CONTAINER_ID=$id
+    aws cloudwatch put-metric-data --namespace "Final-USE-CASE" --metric-name "DockerMEMUtilization" --value $MEM --dimensions CONTAINER_ID=$id
+    echo "Pushed to CloudWatch -> host=$id CPU=$CPU% MEM=$MEM"
+  fi
+done
